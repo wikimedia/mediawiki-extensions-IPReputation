bd1012c8b5d84d86071b2a73af1cbbb7352192d1
Kosta Harlan <kharlan@wikimedia.org>
2024-03-12 14:58:26 +0100

    preauth: Port pre-authentication provider from CentralAuth
    Why:
    
    - The code belongs in IPReputation, not in CentralAuth
    
    What:
    
    - Copy code from CentralAuth's IpReputation PreAuthenticationProvider to
      IPReputation.
    - Minor rewording of config descriptions
    - Clarify the Logstash message shown when in log only mode
    - Default to log-only mode, and being enabled; default to using the WMF
      production URL for ipoid
    
    Bug: T359922
    Change-Id: I6c2dd475f1ce9eae17142a459b6bcff6c143d02a

---
+++ src/PreAuthenticationProvider.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace MediaWiki\Extension\IPReputation;
+
+use ApiMessage;
+use Liuggio\StatsdClient\Factory\StatsdDataFactoryInterface;
+use MediaWiki\Auth\AbstractPreAuthenticationProvider;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Language\FormatterFactory;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\Permissions\PermissionManager;
+use MediaWiki\User\UserIdentity;
+use RequestContext;
+use StatusValue;
+use WANObjectCache;
+use Wikimedia\IPUtils;
+
+/**
+ * PreAuthentication provider that checks if an IP address is known to IPoid
+ *
+ * @see https://wikitech.wikimedia.org/wiki/Service/IPoid
+ */
+class PreAuthenticationProvider extends AbstractPreAuthenticationProvider {
+
+	private HttpRequestFactory $httpRequestFactory;
+	private StatsdDataFactoryInterface $statsDataFactory;
+	private WANObjectCache $cache;
+	private FormatterFactory $formatterFactory;
+	private PermissionManager $permissionManager;
+
+	public function __construct(
+		FormatterFactory $formatterFactory,
+		HttpRequestFactory $httpRequestFactory,
+		WANObjectCache $cache,
+		StatsdDataFactoryInterface $statsDataFactory,
+		PermissionManager $permissionManager
+	) {
+		$this->formatterFactory = $formatterFactory;
+		$this->httpRequestFactory = $httpRequestFactory;
+		$this->cache = $cache;
+		$this->statsDataFactory = $statsDataFactory;
+		$this->permissionManager = $permissionManager;
+		$this->logger = LoggerFactory::getInstance( 'IPReputation' );
+	}
+
+	/** @inheritDoc */
+	public function testForAccountCreation( $user, $creator, array $reqs ) {
+		// If feature flag is off, don't do any checks, let the user proceed.
+		if ( !$this->config->get( 'IPReputationIPoidCheckAtAccountCreation' ) ) {
+			return StatusValue::newGood();
+		}
+
+		if (
+			$this->permissionManager->userHasAnyRight(
+				$creator,
+				'ipblock-exempt',
+			)
+		) {
+			return StatusValue::newGood();
+		}
+
+		$ip = $this->manager->getRequest()->getIP();
+
+		$data = $this->getIPoidDataFor( $user, $ip );
+		if ( !$data ) {
+			// IPoid doesn't know anything about this IP; let the authentication request proceed.
+			return StatusValue::newGood();
+		}
+
+		$shouldLogOnly = $this->config->get( 'IPReputationIPoidCheckAtAccountCreationLogOnly' );
+
+		if ( !isset( $data['risks'] ) || !$data['risks'] ) {
+			// 'risks' should always be set and populated, but if not set to 'UNKNOWN'.
+			$data['risks'] = [ 'UNKNOWN' ];
+		}
+
+		if ( !isset( $data['tunnels'] ) ) {
+			// 'tunnels' should always be set, but if not set to empty list.
+			$data['tunnels'] = [];
+		}
+
+		$risksToBlock = $this->config->get( 'IPReputationIPoidDenyAccountCreationRiskTypes' );
+		$tunnelTypesToBlock = $this->config->get( 'IPReputationIPoidDenyAccountCreationTunnelTypes' );
+
+		$risks = $data['risks'];
+		sort( $risks );
+
+		$tunnels = $data['tunnels'];
+		sort( $tunnels );
+
+		// Allow for the possibility to exclude VPN users from having account
+		// creation denied, if the only risk type known for the IP is that it's a VPN,
+		// and if config is set up to allow VPN tunnel types.
+		// That would be done with:
+		// $wgIPReputationDenyAccountCreationRiskTypes = [ 'TUNNEL', 'CALLBACK_PROXY', ... ];
+		// $wgIPReputationDenyAccountCreationTunnelTypes = [ 'PROXY', 'UNKNOWN' ];
+		// If the only risk type is a TUNNEL...
+		if (
+			$risks === [ 'TUNNEL' ]
+			// and there are tunnels listed for the IP
+			&& count( $tunnels )
+			// and we have configured TUNNEL as a risk type to block
+			&& in_array( 'TUNNEL', $risksToBlock )
+			// and the configured tunnel types to block are *not* present in the data
+			&& !array_intersect( $tunnelTypesToBlock, $tunnels )
+		) {
+			$this->logger->debug(
+				'Allowing account creation for user {user} as IP {ip} is known to IPoid '
+				. 'with only non-blocked tunnels ({tunnelTypes})',
+				[
+					'user' => $user->getName(),
+					'ip' => $ip,
+					'tunnelTypes' => implode( ' ', $tunnels ),
+					'IPoidData' => json_encode( $data ),
+				]
+			);
+			return StatusValue::newGood();
+		}
+
+		// Otherwise, check for other risks.
+		$blockedRisks = array_intersect( $risksToBlock, $risks );
+		if ( $blockedRisks ) {
+			$prefixText = $shouldLogOnly ? '[log only] Would have blocked ' : 'Blocking ';
+			$this->logger->notice(
+				$prefixText . 'account creation for user {user} as IP {ip} is known to IPoid '
+				. 'with risks {riskTypes} (blocked due to {blockedRiskTypes})',
+				[
+					'user' => $user->getName(),
+					'ip' => $ip,
+					'riskTypes' => implode( ' ', $risks ),
+					'blockedRiskTypes' => implode( ' ', $blockedRisks ),
+					'IPoidData' => json_encode( $data ),
+				]
+			);
+
+			$statsdKey = $shouldLogOnly ? 'DenyAccountCreationLogOnly' : 'DenyAccountCreation';
+			$this->statsDataFactory->increment( "IPReputation.$statsdKey." . implode( '_', $risks ) );
+
+			if ( $shouldLogOnly ) {
+				return StatusValue::newGood();
+			}
+
+			return StatusValue::newFatal( ApiMessage::create( 'ipreputation-blocked-ip-reputation', 'autoblocked' ) );
+		}
+
+		return StatusValue::newGood();
+	}
+
+	/**
+	 * @param UserIdentity $user
+	 * @param string $ip
+	 *
+	 * @return array|null IPoid data for the specific address, or null if there is no data
+	 */
+	private function getIPoidDataFor( UserIdentity $user, string $ip ): ?array {
+		$data = $this->cache->getWithSetCallback(
+			$this->cache->makeGlobalKey( 'ipreputation-ipoid', $ip ),
+			// IPoid data is refreshed every 24 hours and roughly 10% of its IPs drop out
+			// of the database each 24-hour cycle. A one hour TTL seems reasonable to allow
+			// no longer problematic IPs to get evicted from the cache relatively quickly,
+			// and also means that IPs for e.g. residential proxies are updated in our cache
+			// relatively quickly.
+			$this->cache::TTL_HOUR,
+			function () use ( $ip, $user ) {
+				// If IPoid URL isn't configured, don't do any checks, let the user proceed.
+				$baseUrl = $this->config->get( 'IPReputationIPoidUrl' );
+				if ( !$baseUrl ) {
+					$this->logger->warning(
+						'Configured to check IP reputation on signup, but no IPoid URL configured'
+					);
+					// Don't cache this.
+					return false;
+				}
+
+				$timeout = $this->config->get( 'IPReputationIPoidRequestTimeoutSeconds' );
+				// Convert IPv6 to lowercase, to match IPoid storage format.
+				$url = $baseUrl . '/feed/v1/ip/' . IPUtils::prettifyIP( $ip );
+				$request = $this->httpRequestFactory->create( $url, [
+					'method' => 'GET',
+					'timeout' => $timeout,
+					'connectTimeout' => 1,
+				] );
+				$response = $request->execute();
+				if ( !$response->isOK() ) {
+					// Probably a 404, which means IPoid doesn't know about the IP.
+					// If not a 404, log it, so we can figure out what happened.
+					if ( $request->getStatus() !== 404 ) {
+						$statusFormatter = $this->formatterFactory->getStatusFormatter( RequestContext::getMain() );
+						[ $errorText, $context ] = $statusFormatter->getPsr3MessageAndContext( $response );
+						$this->logger->error( $errorText, $context );
+					}
+
+					return null;
+				}
+
+				$data = json_decode( $request->getContent(), true );
+
+				if ( !$data ) {
+					// Malformed data.
+					$this->logger->error(
+						'Got invalid JSON data while checking user {user} with IP {ip}',
+						[
+							'ip' => $ip,
+							'user' => $user->getName(),
+							'response' => $request->getContent()
+						]
+					);
+					return null;
+				}
+
+				// IPoid will return the IP in lower case format, and we are searching for the
+				// indexed value in the returned array.
+				if ( !isset( $data[IPUtils::prettifyIP( $ip )] ) ) {
+					// IP should always be set in the data array, but just to be safe.
+					$this->logger->error(
+						'Got JSON data with no IP {ip} present while checking user {user}',
+						[
+							'ip' => $ip,
+							'user' => $user->getName(),
+							'response' => $request->getContent()
+						]
+					);
+					return null;
+				}
+
+				// We have a match and valid data structure;
+				// return the values for this IP for storage in the cache.
+				return $data[$ip];
+			}
+		);
+
+		// Unlike null, false tells cache not to cache something. Normalize both to null before returning.
+		if ( $data === false ) {
+			return null;
+		}
+
+		return $data;
+	}
+}
---
+++ tests/phpunit/integration/PreAuthenticationProviderTest.php
@@ -0,0 +1,391 @@
+<?php
+
+namespace MediaWiki\Extension\IPReputation\Tests\Phpunit\Integration;
+
+use HashBagOStuff;
+use MediaWiki\Auth\AuthManager;
+use MediaWiki\Config\HashConfig;
+use MediaWiki\Extension\IPReputation\PreAuthenticationProvider;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Request\WebRequest;
+use MediaWiki\Tests\Unit\Auth\AuthenticationProviderTestTrait;
+use MediaWiki\User\User;
+use MediaWikiIntegrationTestCase;
+use MockHttpTrait;
+use MWHttpRequest;
+use StatusValue;
+use WANObjectCache;
+
+/**
+ * @covers \MediaWiki\Extension\IPReputation\PreAuthenticationProvider
+ * @group Database
+ */
+class PreAuthenticationProviderTest extends MediaWikiIntegrationTestCase {
+	use MockHttpTrait;
+	use AuthenticationProviderTestTrait;
+
+	public function testTestForAccountCreationDenyIfIPMatchButNoRisksOrTunnels() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( json_encode( [ $ip => [ 'data' ] ] ) );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+				'IPReputationIPoidCheckAtAccountCreationLogOnly' => false,
+				'IPReputationIPoidDenyAccountCreationRiskTypes' => [ 'CALLBACK_PROXY', 'UNKNOWN' ],
+				'IPReputationIPoidDenyAccountCreationTunnelTypes' => [ 'PROXY' ],
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusNotGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return fatal status if IP matches'
+		);
+	}
+
+	public function testTestForAccountCreationMalformedData() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( 'foo' );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return good status if malformed data'
+		);
+	}
+
+	public function testTestForAccountCreationIPNotInData() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( json_encode( [ 'foo' => 'bar' ] ) );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return good status if IP is not in returned data'
+		);
+	}
+
+	public function testTestForAccountCreationTunnelType() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( json_encode( [ '1.2.3.4' => [
+				'risks' => [ 'TUNNEL' ],
+				'tunnels' => [ 'PROXY' ]
+			] ] ) );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+				'IPReputationIPoidDenyAccountCreationRiskTypes' => [ 'TUNNEL' ],
+				'IPReputationIPoidDenyAccountCreationTunnelTypes' => [ 'PROXY' ],
+				'IPReputationIPoidCheckAtAccountCreationLogOnly' => false,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusNotGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return bad status if IP is a proxy tunnel and configured to deny those types.'
+		);
+	}
+
+	public function testTestForAccountCreationTunnelTypeAllowVPNIfDesired() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( json_encode( [ '1.2.3.4' => [
+				'risks' => [ 'TUNNEL' ],
+				'tunnels' => [ 'VPN' ]
+			] ] ) );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+				'IPReputationIPoidDenyAccountCreationRiskTypes' => [ 'TUNNEL', 'GEO_MISMATCH' ],
+				'IPReputationIPoidDenyAccountCreationTunnelTypes' => [ 'PROXY' ],
+				'IPReputationIPoidCheckAtAccountCreationLogOnly' => true,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return good status if IP is a VPN tunnel and app is configured to block only proxies.'
+		);
+	}
+
+	public function testTestForAccountCreationRiskTypesConfig() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( true );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$ip = '1.2.3.4';
+		$mwHttpRequest->method( 'getContent' )
+			->willReturn( json_encode( [ '1.2.3.4' => [
+				'risks' => [ 'TUNNEL', 'GEO_MISMATCH' ],
+				'tunnels' => [ 'PROXY' ]
+			] ] ) );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( $ip );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+				'IPReputationIPoidDenyAccountCreationRiskTypes' => [ 'GEO_MISMATCH' ],
+				'IPReputationIPoidDenyAccountCreationTunnelTypes' => [ 'PROXY' ],
+				'IPReputationIPoidCheckAtAccountCreationLogOnly' => false,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusNotGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Return bad status if IP matches configured risk types'
+		);
+	}
+
+	public function testTestForAccountCreationDoNothingWithoutIPoidUrl() {
+		$httpRequestFactory = $this->createMock( HttpRequestFactory::class );
+		$httpRequestFactory->expects( $this->never() )->method( 'request' );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( '127.0.0.1' );
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$httpRequestFactory,
+			$this->getServiceContainer()->getMainWANObjectCache(),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => null,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Do nothing if IPoid URL is not set'
+		);
+	}
+
+	public function testTestForAccountCreationDoNothingWithoutFeatureFlag() {
+		$httpRequestFactory = $this->createMock( HttpRequestFactory::class );
+		$httpRequestFactory->expects( $this->never() )->method( 'request' );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$httpRequestFactory,
+			$this->getServiceContainer()->getMainWANObjectCache(),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => null,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+			] )
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Do nothing if feature flag is off'
+		);
+	}
+
+	public function testTestForAccountCreationDoNothingIfIPoidHasNoMatch() {
+		$mwHttpRequest = $this->createMock( MWHttpRequest::class );
+		$status = new StatusValue();
+		$status->setOK( false );
+		$mwHttpRequest->method( 'execute' )
+			->willReturn( $status );
+		$this->installMockHttp( $mwHttpRequest );
+		$request = $this->createMock( WebRequest::class );
+		$request->method( 'getIP' )->willReturn( '1.2.3.4' );
+		$provider = new PreAuthenticationProvider(
+			$this->getServiceContainer()->getFormatterFactory(),
+			$this->getServiceContainer()->getHttpRequestFactory(),
+			new WANObjectCache( [ 'cache' => new HashBagOStuff() ] ),
+			$this->getServiceContainer()->getPerDbNameStatsdDataFactory(),
+			$this->getServiceContainer()->getPermissionManager()
+		);
+		$authManager = $this->createMock( AuthManager::class );
+		$authManager->method( 'getRequest' )->willReturn( $request );
+		$this->initProvider(
+			$provider,
+			new HashConfig( [
+				'IPReputationIPoidCheckAtAccountCreation' => true,
+				'IPReputationIPoidUrl' => 'http://localhost:6035',
+				'IPReputationIPoidRequestTimeoutSeconds' => 2,
+			] ),
+			null,
+			$authManager
+		);
+		$this->assertStatusGood(
+			$provider->testForAccountCreation(
+				$this->createMock( User::class ),
+				$this->createMock( User::class ),
+				[]
+			),
+			'Do nothing if IPoid has no match'
+		);
+	}
+}
