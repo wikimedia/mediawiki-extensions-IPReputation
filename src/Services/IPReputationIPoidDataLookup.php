c55d36fa4b3b8f55089bb7023fab8e8faf4cc657
Dreamy Jazz <wpgbrown@wikimedia.org>
2025-04-24 17:38:00 +0100

    Measure time to get IPoid data in IPReputation
    Why:
    * We want to measure the time it takes for IPReptutation to fetch
      IPoid data from the service, along with what code caused the
      lookup.
    * This is so that we can measure the additional time that
      IPReputation variables add to an edit when the variables are used
      in an AbuseFilter.
    * Grafana graphs already exist for the time it takes to run
      AbuseFilters in general, so we don't need to necessarily track
      anything else than how long the request takes in the case of
      a cache miss.
    * We should not collect this data when the call resulted in no
      request to IPoid (i.e. a cache hit).
    ** This is because every AbuseFilter IPReputation variable used
       in the filter causes a call to the service method and so there
       could be tens of calls for each edit (which could overload
       Prometheus).
    ** Furthermore, knowing the time it takes for the code to return
       a cached value isn't important given that it should be
       negligible.
    
    What:
    * Update IPReputationIPoidDataLookup::getIPoidDataForIp to time
      how long it takes to generate the IPoid data on a cache miss,
      storing this under the 'ipoid_data_lookup_time' metric.
    ** To make this easier, the code that makes the HTTP request is
       moved to a private helper method.
    ** This time does not include how long it takes to generate the
       IPoidResponse object. However, that should be negligible.
    ** The timing metric stores the caller to the method as a label
       value to allow filtering based on what code asked for the data.
    *** This will result in label values that contain a longer string,
        but should ensure that no duplications occur in the value used
        to identify the caller.
    *** It appears that the label value can be very long before
        issues occur, so strings of around 100 characters should be
        fine.
    * Add tests for these changes to ensure consistent test coverage.
    
    Bug: T388781
    Change-Id: I7ccab7b3bdb5eaaff4a07335ce1f3e7053dd6f90

--- src/Services/IPReputationIPoidDataLookup.php
+++ src/Services/IPReputationIPoidDataLookup.php
@@ -11,6 +11,7 @@ use Psr\Log\LoggerInterface;
 use RuntimeException;
 use Wikimedia\IPUtils;
 use Wikimedia\ObjectCache\WANObjectCache;
+use Wikimedia\Stats\StatsFactory;
 
 /**
  * Service used to check if an IP address is known to IPoid and if so provide IPReputation data about that
@@ -30,12 +31,14 @@ class IPReputationIPoidDataLookup {
 	private HttpRequestFactory $httpRequestFactory;
 	private WANObjectCache $cache;
 	private FormatterFactory $formatterFactory;
+	private StatsFactory $statsFactory;
 	private LoggerInterface $logger;
 
 	public function __construct(
 		ServiceOptions $options,
 		FormatterFactory $formatterFactory,
 		HttpRequestFactory $httpRequestFactory,
+		StatsFactory $statsFactory,
 		WANObjectCache $cache,
 		LoggerInterface $logger
 	) {
@@ -43,6 +46,7 @@ class IPReputationIPoidDataLookup {
 		$this->options = $options;
 		$this->formatterFactory = $formatterFactory;
 		$this->httpRequestFactory = $httpRequestFactory;
+		$this->statsFactory = $statsFactory;
 		$this->cache = $cache;
 		$this->logger = $logger;
 	}
@@ -65,73 +69,21 @@ class IPReputationIPoidDataLookup {
 			// relatively quickly.
 			$this->cache::TTL_HOUR,
 			function () use ( $ip, $caller ) {
-				// If IPoid URL isn't configured, then raise a warning and return that no data was found.
-				$baseUrl = $this->options->get( 'IPReputationIPoidUrl' );
-				if ( !$baseUrl ) {
-					$this->logger->warning(
-						'IPReputation attempted to query IPoid but the IPoid URL is not ' .
-							'configured when checking IP for {caller}',
-						[ 'caller' => $caller ]
-					);
-					// Don't cache this.
-					return false;
+				$start = microtime( true );
+				$ipoidData = $this->getIPoidDataForIPInternal( $ip, $caller );
+				$delay = microtime( true ) - $start;
+
+				// Track the time it took to make a request to IPoid. We do not do this if we could not cache the
+				// response to avoid overloading the StatsFactory backend as this method gets called frequently.
+				// At the moment the cache is only missed if the IPoid URL is not set.
+				if ( $ipoidData !== false ) {
+					$this->statsFactory->withComponent( 'IPReputation' )
+						->getTiming( 'ipoid_data_lookup_time' )
+						->setLabel( 'caller', $caller )
+						->observeSeconds( $delay );
 				}
 
-				$timeout = $this->options->get( 'IPReputationIPoidRequestTimeoutSeconds' );
-				// Convert IPv6 to lowercase, to match IPoid storage format.
-				$url = $baseUrl . '/feed/v1/ip/' . IPUtils::prettifyIP( $ip );
-				$request = $this->httpRequestFactory->create( $url, [
-					'method' => 'GET',
-					'timeout' => $timeout,
-					'connectTimeout' => 1,
-				], $caller );
-				$response = $request->execute();
-				if ( !$response->isOK() ) {
-					// Probably a 404, which means IPoid doesn't know about the IP.
-					// If not a 404, log it, so we can figure out what happened.
-					if ( $request->getStatus() !== 404 ) {
-						$statusFormatter = $this->formatterFactory->getStatusFormatter( RequestContext::getMain() );
-						$this->logger->error( ...$statusFormatter->getPsr3MessageAndContext( $response, [
-							'exception' => new RuntimeException(),
-						] ) );
-					}
-
-					return null;
-				}
-
-				$data = json_decode( $request->getContent(), true );
-
-				if ( !$data ) {
-					// Malformed data.
-					$this->logger->error(
-						'Got invalid JSON data from IPoid while checking IP {ip} for {caller}',
-						[
-							'ip' => $ip,
-							'response' => $request->getContent(),
-							'caller' => $caller,
-						]
-					);
-					return null;
-				}
-
-				// IPoid will return the IP in lower case format, and we are searching for the
-				// indexed value in the returned array.
-				if ( !isset( $data[IPUtils::prettifyIP( $ip )] ) ) {
-					// IP should always be set in the data array, but just to be safe.
-					$this->logger->error(
-						'Got JSON data from IPoid missing the requested IP while checking {ip} for {caller}',
-						[
-							'ip' => $ip,
-							'response' => $request->getContent(),
-							'caller' => $caller,
-						]
-					);
-					return null;
-				}
-
-				// We have a match and valid data structure;
-				// return the values for this IP for storage in the cache.
-				return $data[$ip];
+				return $ipoidData;
 			}
 		);
 
@@ -142,5 +94,85 @@ class IPReputationIPoidDataLookup {
 
 		// Return the IPoid data wrapped in the value object for ease of access for the caller.
 		return IPoidResponse::newFromArray( $data );
+	}
+
+	/**
+	 * Helper for {@link self::getIPoidDataForIp} that does not cache the response from IPoid, and returns
+	 * a value that can be returned from the callback provided to {@link WANObjectCache::getWithSetCallback}.
+	 *
+	 * @param string $ip See {@link self::getIPoidDataForIp}
+	 * @param string $caller See {@link self::getIPoidDataForIp}
+	 * @return array|false|null The value to be returned and also cached. If false then the value should not be
+	 *   cached and interpreted as null for other code.
+	 */
+	private function getIPoidDataForIPInternal( string $ip, string $caller ) {
+		// If IPoid URL isn't configured, then raise a warning and return that no data was found.
+		$baseUrl = $this->options->get( 'IPReputationIPoidUrl' );
+		if ( !$baseUrl ) {
+			$this->logger->warning(
+				'IPReputation attempted to query IPoid but the IPoid URL is not ' .
+				'configured when checking IP for {caller}',
+				[ 'caller' => $caller ]
+			);
+			// Don't cache this.
+			return false;
+		}
+
+		$timeout = $this->options->get( 'IPReputationIPoidRequestTimeoutSeconds' );
+		// Convert IPv6 to lowercase, to match IPoid storage format.
+		$url = $baseUrl . '/feed/v1/ip/' . IPUtils::prettifyIP( $ip );
+		$request = $this->httpRequestFactory->create( $url, [
+			'method' => 'GET',
+			'timeout' => $timeout,
+			'connectTimeout' => 1,
+		], $caller );
+		$response = $request->execute();
+
+		if ( !$response->isOK() ) {
+			// Probably a 404, which means IPoid doesn't know about the IP.
+			// If not a 404, log it, so we can figure out what happened.
+			if ( $request->getStatus() !== 404 ) {
+				$statusFormatter = $this->formatterFactory->getStatusFormatter( RequestContext::getMain() );
+				$this->logger->error( ...$statusFormatter->getPsr3MessageAndContext( $response, [
+					'exception' => new RuntimeException(),
+				] ) );
+			}
+
+			return null;
+		}
+
+		$data = json_decode( $request->getContent(), true );
+
+		if ( !$data ) {
+			// Malformed data.
+			$this->logger->error(
+				'Got invalid JSON data from IPoid while checking IP {ip} for {caller}',
+				[
+					'ip' => $ip,
+					'response' => $request->getContent(),
+					'caller' => $caller,
+				]
+			);
+			return null;
+		}
+
+		// IPoid will return the IP in lower case format, and we are searching for the
+		// indexed value in the returned array.
+		if ( !isset( $data[IPUtils::prettifyIP( $ip )] ) ) {
+			// IP should always be set in the data array, but just to be safe.
+			$this->logger->error(
+				'Got JSON data from IPoid missing the requested IP while checking {ip} for {caller}',
+				[
+					'ip' => $ip,
+					'response' => $request->getContent(),
+					'caller' => $caller,
+				]
+			);
+			return null;
+		}
+
+		// We have a match and valid data structure;
+		// return the values for this IP for storage in the cache.
+		return $data[$ip];
 	}
 }
