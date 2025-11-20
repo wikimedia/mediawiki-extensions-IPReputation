925541d599bf3210ed335c83db09fa97eb1800f7
Kosta Harlan <kharlan@wikimedia.org>
2025-11-20 13:17:59 +0100

    maintenance: Add a GetIPReputationData script
    Why:
    
    - Facilitates local testing
    
    What:
    
    - Add a maintenance script, copy/pasted more or less from IPInfo's
      InfoManager maintenance script
    
    Bug: T410615
    Change-Id: I7501f156497e8e850a7a9eb0eb863ec98103b463

---
+++ maintenance/GetIPReputationData.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MediaWiki\IPReputation\Maintenance;
+
+use MediaWiki\Extension\IPReputation\Services\IPReputationIPoidDataLookup;
+use MediaWiki\Json\FormatJson;
+use MediaWiki\Maintenance\Maintenance;
+use Wikimedia\IPUtils;
+
+// Light-weight maintenance script for local development (or QA testing in production), does not need testing coverage.
+// @codeCoverageIgnoreStart
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+class GetIPReputationData extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Retrieve data for an IP address.' );
+		$this->addArg( 'ip', 'The IP address to use in the lookup.' );
+		$this->requireExtension( 'IPReputation' );
+	}
+
+	public function execute() {
+		$ip = $this->getArg( 1 );
+		if ( !IPUtils::isValid( $ip ) ) {
+			$this->fatalError( "\"$ip\" is not a valid IP address." );
+		}
+		/** @var IPReputationIPoidDataLookup $IPReputationIPoidDataLookup */
+		$IPReputationIPoidDataLookup = $this->getServiceContainer()->getService( 'IPReputationIPoidDataLookup' );
+		$result = $IPReputationIPoidDataLookup->getIPoidDataForIp( $ip, __METHOD__ );
+		if ( !$result ) {
+			$this->output( "No result found" . PHP_EOL );
+			return;
+		}
+		$this->output( FormatJson::encode( $result ) . PHP_EOL );
+	}
+}
+
+$maintClass = GetIPReputationData::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
+// @codeCoverageIgnoreEnd
